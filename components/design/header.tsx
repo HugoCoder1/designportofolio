31a52bcad043f6a4b6e1bc31d8a6c1395696eec6
HugoCoder1 <hugueshugues2002@gmail.com>
2025-01-12 09:14:07 +0100

    modif

--- components/design/header.tsx
+++ components/design/header.tsx
@@ -14,15 +14,15 @@ function Header() {
 
   // Ferme le menu si on clique en dehors
   useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (
-        isMenuOpen &&
-        menuRef.current &&
-        !menuRef.current.contains(event.target)
-      ) {
-        setIsMenuOpen(false);
-      }
-    };
+     const handleClickOutside = (event: MouseEvent) => {
+       if (
+         isMenuOpen &&
+         menuRef.current &&
+         !menuRef.current.contains(event.target as Node)
+       ) {
+         setIsMenuOpen(false);
+       }
+     };
 
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
